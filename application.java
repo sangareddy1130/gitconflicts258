87d23d4c5ccfff7dc947a773cdae94b2135af8cd
sangareddy <sangareddy1130@gmail.com>
2023-07-16 15:17:24 +0530

    file added for development

---
+++ application.java
@@ -0,0 +1,17 @@
+public class GFG {
+    public static void main(String[] args)
+    {
+        try {
+            FileWriter Writer
+                = new FileWriter("myfile.txt");
+            Writer.write(
+                "Files in Java are seriously good!!");
+            Writer.close();
+            System.out.println("Successfully written.");
+        }
+        catch (IOException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
+}
